4f634c2ac288765650d7c1f195738b77a555f237
vikkisupurrbe <yingjialiu1216@gmail.com>
2025-04-14 16:55:35 -0700

    Implement turn

---
+++ classes/turn.rb
@@ -0,0 +1,21 @@
+require './question'
+
+class Turn
+  def initialize(player)
+    @player = player
+    @question = Question.new
+  end
+
+  def execute
+    puts "#{@player.name}: #{@question.text}"
+    print "> "
+    input = gets.chomp
+
+    if @question.correct?(input)
+      puts "#{@player.name}: YES! You are correct."
+    else
+      puts "#{@player.name}: Seriously? No!"
+      @player.lose_life
+    end
+  end
+end
